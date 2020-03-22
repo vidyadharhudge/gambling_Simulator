044ad337034b839872f3bafcd57a1bac2d290f7d
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-22 14:40:36 +0530

    Added:-gambler either win or loss

--- gambling.sh
+++ gambling.sh
@@ -1,5 +1,14 @@
-#!/bin/bash -x
+#!/bin/bash 
 echo  "Welcome to gambling simulator game"
 stake=100
 bet=1
-
+gambler=$((RANDOM%2))
+if [ $gambler -eq 0 ]
+then
+      stake=$((stake+1))
+      echo "Win"
+else
+      stake=$((stake-1))
+      echo "loss"
+fi
+echo $stake
