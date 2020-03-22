7ce2a22a0147c30f6d451ba639bf63931fefede3
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-22 15:42:21 +0530

    Added:-Total winning and lossing amount after 20 days

--- gambling.sh
+++ gambling.sh
@@ -1,16 +1,27 @@
-#!/bin/bash 
+#!/bin/bash -x  
 echo  "Welcome to gambling simulator game"
 stake=100
 bet=1
-
-while (( stake<150 && stake>50 ))
+total_Winning_amount=0
+total_Loss_amount=0
+day=0
+while (( $day<20 ))
 do
-     gambler=$((RANDOM%2))
-     if [ $gambler -eq 0 ]
-     then
-          stake=$((stake+1))
-     else
-          stake=$((stake-1))
-     fi
+   while (( stake<150 && stake>50 ))
+   do
+       gambler=$((RANDOM%2))
+       if [ $gambler -eq 0 ]
+       then
+            stake=$((stake+bet))
+            total_Winning_amount=$((total_Winning_amount+bet))
+       else
+            stake=$((stake-bet))
+            total_Loss_amount=$((total_loss_amount+1))
+       fi
+         
+   done
+   day+=1x
 done
 echo $stake
+echo "total win is:$total_Winning_amount"
+echo "total loss is:$total_Loss_amount"
