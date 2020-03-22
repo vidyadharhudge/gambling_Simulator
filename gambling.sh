b9976dffe452147c652f671ac9bfb7df25007c7e
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-22 15:04:34 +0530

    Added:-Calculate resigin for day

--- gambling.sh
+++ gambling.sh
@@ -2,13 +2,15 @@
 echo  "Welcome to gambling simulator game"
 stake=100
 bet=1
-gambler=$((RANDOM%2))
-if [ $gambler -eq 0 ]
-then
-      stake=$((stake+1))
-      echo "Win"
-else
-      stake=$((stake-1))
-      echo "loss"
-fi
+
+while (( stake<150 && stake>50 ))
+do
+     gambler=$((RANDOM%2))
+     if [ $gambler -eq 0 ]
+     then
+          stake=$((stake+1))
+     else
+          stake=$((stake-1))
+     fi
+done
 echo $stake
