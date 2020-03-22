18a1c35273eb5dab33d3743252cb172c74189ee8
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-22 22:35:52 +0530

    Added:-Count max luckyiest and unluckyiest day

--- gambling.sh
+++ gambling.sh
@@ -4,6 +4,37 @@ stake=100
 bet=15
 total_Winning_amount=0
 total_Loss_amount=0
+
+function luckyDay()
+{
+     local maximumLucky=$1
+     for (( days=1; days<=20; days++ ))
+     do
+        if [[ ${Totalwingamble[$days]} -gt $maximumLucky ]]
+        then
+           maximumLucky=${Totalwingamble[$days]} 
+           luckyiestday=$days
+        fi
+    done
+    echo "max luckyiestday is:$luckyiestday"
+
+
+}
+
+function unluckyDay()
+{
+      local maximumUnlucky=$1
+      for (( days=1; days<=20; days++ ))
+      do
+            if [[ ${Totallostgamble[$days]} -gt $maximumUnLucky ]]
+            then
+               maximumUnLucky=${Totallostgamble[$days]}
+               Unluckyiestday=$days
+            fi
+      done
+   echo "max Unluckiestday is:$Unluckyiestday"
+}
+
 for (( days=1; days<=20; days++ ))
 do
         TotaldayWin=0
@@ -33,3 +64,7 @@ echo "total win is:$total_Winning_amount"
 echo "total loss is:$total_Loss_amount"
 echo "days:${!Totalwingamble[@]} win:${Totalwingamble[@]}"
 echo "days:${!Totallostgamble[@]} lost:${Totallostgamble[@]}"
+luckDay=$( luckyDay ${Totalwingamble[1]} )
+unluckDay=$( unluckyDay ${Totallostgamble[1]} ) 
+echo "$luckDay"
+echo "$unluckDay"
