385cef5cabac3a991c751d93dde07dc10a847d23
vidyadharhudge <vidyadharhudge1997@gmail.com>
2020-03-24 15:39:37 +0530

    Added:-Playing next month or stop gambling

--- gambling.sh
+++ gambling.sh
@@ -1,77 +1,77 @@
 #!/bin/bash   
 echo  "Welcome to gambling simulator game"
-bet=15
+BET=15
 
 stake=100
-total_Winning_amount=0
-total_Loss_amount=0
-player=1
+Total_Winning_Amount=0
+Total_Loss_Amount=0
+Player=1
 
-function luckyDay()
+function LuckyDay()
 {
-     local maximumLucky=$1
+     local MaxiMumLucky=$1
      for (( days=1; days<=20; days++ ))
      do
-        if [[ ${Totalwingamble[$days]} -gt $maximumLucky ]]
+        if [[ ${TotalWinGamble[$days]} -gt $MaxiMumLucky ]]
         then
-           maximumLucky=${Totalwingamble[$days]} 
-           luckyiestday=$days
+           MaxiMumLucky=${TotalWinGamble[$days]} 
+           LuckyiestDay=$days
         fi
     done
-    echo "max luckyiestday is:$luckyiestday"
+    echo "max luckyiestday is:$LuckyiestDay"
 
 
 }
 
-function unluckyDay()
+function UnluckyDay()
 {
-      local maximumUnlucky=$1
+      local MaxiMumUnLucky=$1
       for (( days=1; days<=20; days++ ))
       do
-            if [[ ${Totallostgamble[$days]} -gt $maximumUnLucky ]]
+            if [[ ${TotalLostGamble[$days]} -gt $MaxiMumUnLucky ]]
             then
-               maximumUnLucky=${Totallostgamble[$days]}
-               Unluckyiestday=$days
+               MaxiMumUnLucky=${TotalLostGamble[$days]}
+               UnLuckyiestDay=$days
             fi
      done
-     echo "max Unluckiestday is:$Unluckyiestday"
+     echo "max Unluckiestday is:$UnLuckyiestDay"
 }
 
-while [[ $player -eq 1 ]]
+while [[ $Player -eq 1 ]]
 do
    for (( days=1; days<=20; days++ ))
    do
-          TotaldayWin=0
-          TotaldayLost=0
+          TotalDayWin=0
+          TotalDayLost=0
           stakeperday=100
       while (( stakeperday<150 && stakeperday>50 ))
       do
           gambler=$((RANDOM%2))
           if [ $gambler -eq 0 ]
           then
-             stake=$((stake+bet))
-             stakeperday=$((stakeperday+bet))
-             total_Winning_amount=$((total_Winning_amount+bet))
-             TotaldayWin=$((TotaldayWin+bet))
+             stake=$((stake+BET))
+             stakeperday=$((stakeperday+BET))
+             Total_Winning_Amount=$((Total_Winning_Amount+BET))
+             TotalDayWin=$((TotalDayWin+BET))
           else
-             stake=$((stake-bet))
-             stakeperday=$((stakeperday-bet))
-             total_Loss_amount=$((total_Loss_amount+1))
-             TotaldayLost=$((TotaldayLost+bet))
+             stake=$((stake-BET))
+             stakeperday=$((stakeperday-BET))
+             Total_Loss_Amount=$((Total_Loss_Amount+1))
+             TotalDayLost=$((TotalDayLost+BET))
           fi
           done
-             Totalwingamble[$days]=$TotaldayWin
-             Totallostgamble[$days]=$TotaldayLost 
+             TotalWinGamble[$days]=$TotalDayWin
+             TotalLostGamble[$days]=$TotalDayLost 
 done
 echo $stake
-echo "total win is:$total_Winning_amount"
-echo "total loss is:$total_Loss_amount"
-echo "days:${!Totalwingamble[@]} win:${Totalwingamble[@]}"
-echo "days:${!Totallostgamble[@]} lost:${Totallostgamble[@]}"
-luckDay=$( luckyDay ${Totalwingamble[1]} )
-unluckDay=$( unluckyDay ${Totallostgamble[1]} ) 
-echo "$luckDay"
-echo "$unluckDay"
-read -p "enter the 1  to continue the gambling game"  playagain
-player=$playagain
+echo "total win is:$Total_Winning_Amount"
+echo "total loss is:$Total_Loss_Amount"
+echo "days:${!TotalWinGamble[@]} win:${TotalWinGamble[@]}"
+echo "days:${!TotalLostGamble[@]} lost:${TotalLostGamble[@]}"
+LuckDay=$( LuckyDay ${TotalWinGamble[1]} )
+UnluckDay=$( UnluckyDay ${TotalLostGamble[1]} ) 
+echo "$LuckDay"
+echo "$UnluckDay"
+read -p "enter the 1  to continue the gambling game" PlayAgain
+Player=$PlayAgain
 done
